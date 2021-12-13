e53d7d17247c451dfca0886e29a052130fecafb8
Trsh16 <tlikorawung@student.ciputra.ac.id>
2021-12-13 09:31:21 +0800

    Add project files.

---
+++ Praktikum14/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum14
+{
+    class Program
+    {
+        public static void notprime()
+        {
+            Console.SetCursorPosition(4, 2);
+            Console.Write("1");
+            Console.SetCursorPosition(9, 2);
+            Console.Write("4");
+            Console.SetCursorPosition(14, 2);
+            Console.Write("6");
+            Console.SetCursorPosition(19, 2);
+            Console.Write("8");
+            Console.SetCursorPosition(24, 2);
+            Console.Write("9");
+            Console.SetCursorPosition(29, 2);
+            Console.Write("10");
+            Console.SetCursorPosition(34, 2);
+            Console.Write("12");
+        }
+        public static void prime()
+        {
+            Console.SetCursorPosition(4, 2);
+            Console.Write("2");
+            Console.SetCursorPosition(9, 2);
+            Console.Write("3");
+            Console.SetCursorPosition(14, 2);
+            Console.Write("5");
+            Console.SetCursorPosition(19, 2);
+            Console.Write("7");
+            Console.SetCursorPosition(24, 2);
+            Console.Write("11");
+            Console.SetCursorPosition(29, 2);
+            Console.Write("13");
+            Console.SetCursorPosition(34, 2);
+            Console.Write("17");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Input : ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            string hasil;
+            int bil = 0;
+            Console.WriteLine("Output : ");
+            for (int i = 1; i <= N; i++)
+            {
+                int angka = N % i;
+                if (angka == 0)
+                {
+                    bil = bil + 1;
+                }
+                else
+                {
+                    bil = bil;
+                }
+            }
+            if (bil > 2 || bil == 1)
+            {
+                notprime();
+            }
+            else
+            {
+                prime();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Praktikum14/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Praktikum14")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Praktikum14")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("356e11f7-88a1-4dc0-85dd-ba6f2ab7144b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
